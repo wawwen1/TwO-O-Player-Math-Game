3a022658aaa93d30cbda9a19a90ba560bf276861
warren roque <warrenroque17@gmail.com>
2022-05-05 04:15:14 +0000

    intialize sum & add answer check method + new question method

--- questions.rb
+++ questions.rb
@@ -1,15 +1,27 @@
 class Questions
 
-  attr_accessor :number_one :number_two
+  attr_reader :number_one :number_two
 
   def initialize
     @number_one = rand(1..20)
     @number_two = rand(1..20)
+    @sum = @number_one + @number_two
   end
 
-  def question_prompt(player_turn)
-    puts "What does #{number_one} plus #{number_two} equal?"
-    sum = @number_one + @number_two
-    return sum
+  def question_prompt(name)
+    puts "#{name}: What does #{number_one} plus #{number_two} equal?"
   end
+
+  def answer_check?(answer)
+    @sum == answer
+  end
+
+  def question_new
+    question = Questions.new
+    question.question_prompt(name)
+    @answer = $stdin.gets.chomp.to_i
+
+    question.answer_check?(@answer) ? puts "YES! You are correct." : puts "Seriously? No."
+  end 
+
 end
